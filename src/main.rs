8dc0edcbfd8a90785536f8bb62f734f5cfe1671a
Alisa <alisadorofeeva1009@gmail.com>
2026-09-23 01:09:22 +0300

    complete homework

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,13 @@ use std::io;
 fn main() {
     let mut string = String::new();
 
-    io::stdin()
-        .read_line(&mut string)
-        .expect("error");
-    
-    let mut numbers = string
-        .split_whitespace();
-    
-    let a: i64 = numbers
-        .next()
-        .expect("error")
-        .parse()
-        .expect("error");
-    
-    let b: i64 = numbers
-        .next()
-        .expect("error")
-        .parse()
-        .expect("error");
-    
-    println!("{}", a+b);
+    io::stdin().read_line(&mut string).expect("error");
+
+    let mut numbers = string.split_whitespace();
+
+    let a: i64 = numbers.next().expect("error").parse().expect("error");
+
+    let b: i64 = numbers.next().expect("error").parse().expect("error");
+
+    println!("{}", a + b);
 }
